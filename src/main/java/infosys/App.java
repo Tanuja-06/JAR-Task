9cf69b8c3fdbf71bc041f6d636c3d1b5e4e6655d
Tanuja-06 <tanuammu214@gmail.com>
2024-09-27 14:47:50 +0530

    Update App.java

--- src/main/java/infosys/App.java
+++ src/main/java/infosys/App.java
@@ -6,5 +6,8 @@ package infosys;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
+        
     }
 }
